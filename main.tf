7069bd9c0e5cd38173a3076d8b88370779a15b1b
wade730 <wade730@gmail.com>
2024-06-21 10:27:12 -0400

    imported s3 bucket and added state locking

--- main.tf
+++ main.tf
@@ -2,7 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-#terraform import { 
-# import my s3 bucket     
-#}
+resource "aws_s3_bucket" "chummbucket" {
+  bucket = "chummbucket"
+lifecycle {
+    prevent_destroy = true
+  }
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,31 @@
 terraform {
-  backend "s3" {
-    bucket = "chummbucket"
-    key    = "terraform-state"
-    region = "us-east-1"
-  }
-}
-
-terraform {
   required_version = "1.7.5" # declares required terraform version
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.43.0" # declares required version of the aws provider 
     }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "chummbucket"
+    key            = "terraform-state"
+    # dynamodb_table = "terraform-state-lock-dynamo"
+    encrypt        = true
+    region         = "us-east-1"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
