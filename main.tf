312a583c060468b5b6aa97a384b25192f5abe1e1
wade730 <69596376+wade730@users.noreply.github.com>
2024-06-26 14:17:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,26 +4,39 @@ provider "aws" {
 
 resource "aws_s3_bucket" "chummbucket" {
   bucket = "chummbucket"
-lifecycle {
+  lifecycle {
     prevent_destroy = true
   }
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = "chummbucket"
+  versioning_configuration {
+    status = "Enabled"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = "chummbucket"
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = "chummbucket"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform-state-lock-dynamo" {
+  name         = "terraform-state-lock-dynamo"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
 
   attribute {
     name = "LockID"
